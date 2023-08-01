5b989b1021dcf65c41a5f5cf107bbccf5f73a40b
sevdenurdag <83172181+sevdenurdag@users.noreply.github.com>
2023-08-01 21:53:00 +0300

    Add files via upload
    güncelleme yapıldı

---
+++ webprogramlama/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication11.Models;
+
+namespace WebApplication11.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+          if (ModelState.IsValid)
+        {
+            // login işlemleri..
+         }
+           return View(model);
+         }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model) 
+        {
+        
+            if (ModelState.IsValid)
+            {
+                //register işlemleri..
+            }
+            return View(model);
+        }
+       
+
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+    }
+}
