fb62e60962accd5e79e75f78c80f2596ae32edb6
EsraAsci <83405067+EsraAsci@users.noreply.github.com>
2023-08-06 21:14:48 +0300

    Add files via upload
    LoginViewModel.cs güncellemeleri yapıldı

---
+++ webprogramlama/Models/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication12.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(30, ErrorMessage = "Username can be max 30 characters.")]
+        public string Username { get; set; }
+
+        //[DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password can be min 6 characters.")]
+        [MaxLength(16, ErrorMessage = "Password can be max 16 characters.")]
+        public string Password { get; set; }
+    }
+
+
+
+
+}
+
