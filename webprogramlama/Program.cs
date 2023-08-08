8443a7675fb13e9ad8420f2554979257294c3849
sevdenurdag <83172181+sevdenurdag@users.noreply.github.com>
2023-08-08 13:55:58 +0300

    Add files via upload

--- webprogramlama/Program.cs
+++ webprogramlama/Program.cs
@@ -1,4 +1,10 @@
-namespace webprogramlama
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using WebApplication12.Entities;
+
+
+namespace WebApplication12
 {
     public class Program
     {
@@ -7,7 +13,28 @@ namespace webprogramlama
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            builder.Services.AddDbContext<DatabaseContext>(opts =>
+            {
+                opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+                
+            });
+            builder.Services.AddControllers();
+
+
+            builder.Services
+                .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(opts =>
+                {
+                    opts.Cookie.Name = "WebApplication12.auth";
+                    opts.ExpireTimeSpan = TimeSpan.FromDays(7);
+                    opts.SlidingExpiration = false;
+                    opts.LoginPath= "/Account/Login";
+                    opts.LogoutPath = "/Account/Logout";
+                    opts.AccessDeniedPath = "/Home/AccessDenied";
+
+
+                });
 
             var app = builder.Build();
 
@@ -15,15 +42,10 @@ namespace webprogramlama
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
